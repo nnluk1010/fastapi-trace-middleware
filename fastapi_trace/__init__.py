99e3b4c742df31fd98d5b3d198cf99e2c6ddfa42
Nelson <slnluk@hkuspace.hku.hk>
2025-07-15 17:20:54 +0800

    first commit

---
+++ fastapi_trace/__init__.py
@@ -0,0 +1,13 @@
+from .middleware import TraceMiddleware
+from .context import get_trace_id, trace_id_ctx
+from .logging_filters import TraceIdFilter
+from .config import setup_trace_logging
+
+__version__ = "1.0.0"
+__all__ = [
+    "TraceMiddleware", 
+    "get_trace_id", 
+    "trace_id_ctx", 
+    "TraceIdFilter",
+    "setup_trace_logging"
+]
